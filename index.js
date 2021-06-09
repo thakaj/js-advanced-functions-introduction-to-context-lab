d169fa0926a60a7e5b2152192132464896cbe3a1
thakaj <taulant.hakaj@scranton.edu>
2021-06-09 16:39:19 -0400

    commit tree be9a437893eab85b639c715e94afd5f6fada72d9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){        
+    const testEmployee = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }   
+    return testEmployee
+}
+
+function createEmployeeRecords(inputArray){
+    const employeeArray = [];
+    for(let i = 0; i < inputArray.length; i++){
+        employeeArray.push(createEmployeeRecord(inputArray[i]))
+    }return employeeArray
+}
+
+function createTimeInEvent(employee,time){
+    const splitTime = time.split(" ")
+    const d = {
+         type: "timeIm",
+         date: splitTime[0],
+         hour: splitTime[1],
+    }
+    employee.timeInEvents.push(d)
+    return employee
+}
